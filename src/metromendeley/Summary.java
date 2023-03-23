e61b8800fdc7fc943898ae1d1e4286d304f32e69
Andres Goncalves <88066799+andresgoncalves@users.noreply.github.com>
2023-03-23 16:19:07 -0400

    Added keyword appearence count

--- src/metromendeley/Summary.java
+++ src/metromendeley/Summary.java
@@ -1,51 +1,46 @@
 package metromendeley;
 
+import java.util.regex.Pattern;
+
 /**
  * Clase Resumen
  * @author Andres
  */
 public class Summary {
-    private String title;
-    private String body;
-    private String[] authors;
-    private String[] keywords;
+    private final String title;
+    private final String body;
+    private final String[] authors;
+    private final String[] keywords;
+    private final int[] keywordAppearances;
 
     public Summary(String title, String body, String[] authors, String[] keywords) {
         this.title = title;
         this.body = body;
         this.authors = authors;
         this.keywords = keywords;
-    }
-    
-    public String getTitle() {
-        return title;
+        this.keywordAppearances = new int[keywords.length];
+        for(int i = 0; i < keywords.length; i++) {
+            keywordAppearances[i] = (int) Pattern.compile("(^|\\s)\\s*" + Pattern.quote(keywords[i]) + "\\s*(\\s|$)").matcher(body).results().count();
+        }
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public String getTitle() {
+        return title;
     }
 
     public String getBody() {
         return body;
     }
 
-    public void setBody(String body) {
-        this.body = body;
-    }
-
     public String[] getAuthors() {
         return authors;
-    }
-
-    public void setAuthors(String[] authors) {
-        this.authors = authors;
     }
 
     public String[] getKeywords() {
         return keywords;
     }
 
-    public void setKeywords(String[] keywords) {
-        this.keywords = keywords;
+    public int[] getKeywordAppearances() {
+        return keywordAppearances;
     }
 }
